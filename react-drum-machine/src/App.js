4fd6050955a15e78ea95a3af3e9bbbb21d4e9dfb
Ben Hedgepeth <airmaxmoney@protonmail.com>
2020-08-02 21:25:20 -0700

    Defined Components

--- react-drum-machine/src/App.js
+++ react-drum-machine/src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Panel extends React.Component {
+
+}
+
+
+class DrumPad extends React.Component {
+
+  render() {
+    return (
+      <li className="drum-pad">
+        <audio muted={this.props.power}></audio>
+        {this.props.name}
+      </li>
+    )
+  }
+}
+
+class DrumMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active : true
+    };
+  }
+  render() {
+    let items = ['A', 'B', 'C'];
+    let drumPads = items.map((button) => {
+      return <DrumPad name={button} power={!this.state.active} />
+    })
+    return (
+      <div id="drum-machine">
+        <ul class="pads_wrapper">
+          {drumPads}
+        </ul>
+      </div>
+    )
+  }
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <DrumMachine />
 }
 
 export default App;
