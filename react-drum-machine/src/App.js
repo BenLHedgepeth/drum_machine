7dfc919e9d7d4317d04bf996678ccb3e7cfc3f55
Ben Hedgepeth <airmaxmoney@protonmail.com>
2020-08-04 18:44:12 -0700

    Added off/on State functionality

--- react-drum-machine/src/App.js
+++ react-drum-machine/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 
 class Panel extends React.Component {
@@ -11,32 +11,54 @@ class Panel extends React.Component {
 class DrumPad extends React.Component {
 
   render() {
+    let status = this.props.power ? "active" : "inactive";
+    console.log(status);
     return (
-      <li className="drum-pad">
-        <audio muted={this.props.power}></audio>
+      <li className={`drum-pad ${status}`}>
+        <audio muted={!this.props.power}></audio>
         {this.props.name}
       </li>
     )
   }
 }
 
+
 class DrumMachine extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active : true
+      powerOn : false
     };
+    this.useMachine = this.useMachine.bind(this);
   }
+
+  useMachine(event) {
+    if (!this.state.powerOn) {
+      this.setState((state) => ({
+        powerOn: true
+      }))
+      event.target.style.boxShadow = "inset 0px 10px 10px red";
+    } else {
+      this.setState((state) => ({
+        powerOn: false
+      }))
+      event.target.style.boxShadow = "none";
+    }
+  }
+  
   render() {
-    let items = ['A', 'B', 'C'];
+    let items = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
     let drumPads = items.map((button) => {
-      return <DrumPad name={button} power={!this.state.active} />
+      return <DrumPad name={button} power={this.state.powerOn} />
     })
     return (
       <div id="drum-machine">
-        <ul class="pads_wrapper">
+        <ul className="pads_wrapper">
           {drumPads}
         </ul>
+        <div className="controls">
+          <p className="power_button" onClick={this.useMachine}>Power</p>
+        </div>
       </div>
     )
   }
